d7440278994a251e1dfbed8316fd3ead1ded6b49
MuhammadAnees1 <chanees281@gmail.com>
2023-06-07 16:20:37 +0500

    Initial commit

---
+++ app/src/main/java/com/example/a2dgame/Bullet.java
@@ -0,0 +1,35 @@
+package com.example.a2dgame;
+
+import static com.example.a2dgame.GameView.screenRatioX;
+import static com.example.a2dgame.GameView.screenRatioY;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+public class Bullet {
+    int x, y, width, height;
+    Bitmap bullet;
+    Bullet (Resources res) {
+
+        bullet = BitmapFactory.decodeResource(res, R.drawable.bullet);
+
+        width = bullet.getWidth();
+        height = bullet.getHeight();
+
+        width /= 4;
+        height /= 4;
+
+        width = (int) (width * screenRatioX);
+        height = (int) (height * screenRatioY);
+
+        bullet = Bitmap.createScaledBitmap(bullet, width, height, false);
+
+    }
+
+    Rect getCollisionShape () {
+        return new Rect(x, y, x + width, y + height);
+    }
+
+}
---
+++ app/src/main/java/com/example/a2dgame/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.a2dgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean isMute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+            }
+        });
+
+        TextView highScoreTxt = findViewById(R.id.highScoreTxt);
+
+        final SharedPreferences prefs = getSharedPreferences("game", MODE_PRIVATE);
+
+        highScoreTxt.setText("HighScore: " + prefs.getInt("highscore", 0));
+
+        isMute = prefs.getBoolean("isMute", false);
+
+        final ImageView volumeCtrl = findViewById(R.id.volumeCtrl);
+
+        if (isMute)
+            volumeCtrl.setImageResource(R.drawable.baseline_volume_off_24);
+        else
+            volumeCtrl.setImageResource(R.drawable.baseline_volume_up_24);
+
+        volumeCtrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                isMute = !isMute;
+                if (isMute)
+                    volumeCtrl.setImageResource(R.drawable.baseline_volume_off_24);
+                else
+                    volumeCtrl.setImageResource(R.drawable.baseline_volume_up_24);
+
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean("isMute", isMute);
+                editor.apply();
+
+            }
+        });
+
+    }
+}
